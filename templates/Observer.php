ef84dd5bdc92b227493fcc8ab61868871e843b9f
megatr0n <abehinds@msn.com>
2017-10-09 02:30:29 -0600

    Signed-off-by: megatr0n <abehinds@msn.com>

--- templates/Observer.php
+++ templates/Observer.php
@@ -8,7 +8,7 @@ class companyname_modulename_Model_Observer
 	$current_script_path = realpath(dirname(__FILE__)).'/';		
 	//require_once($current_script_path.'../../common_code.php');
 	$datacollection_arr = array();
-	$datacollection_arr = $observer->getEvent()->Datacollection_key()
+	$datacollection_arr = $observer->getEvent()->getDatacollection_key()
         if ($validity == 'true') 
         {
         require_once($current_script_path.'action_method_name.php');
